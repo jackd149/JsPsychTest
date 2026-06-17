d19f209586655091e36b6cac5f733201e1e877c7
jackd149 <jackdurgin149@gmail.com>
2026-06-17 11:30:11 -0400

    Testing warmup trial

--- experiment.js
+++ experiment.js
@@ -13,6 +13,102 @@ const NatureCloseHtml = `<img src="img/NatureClose.png" alt="Nature Close" style
 
 var timeline = [];
 
+//warmup 1 trial
+// things that change for square left vs right
+const square_left = {
+    stimulus: "https://raw.githubusercontent.com/efosterhanson/CHS_stims/main/ProjectSprouts/warmup_intro_trial1_c1.m4v",
+    id_left: "triangle",
+    id_right: "square"
+};
+const square_right = {
+    stimulus: "https://raw.githubusercontent.com/efosterhanson/CHS_stims/main/ProjectSprouts/warmup_intro_trial1_c2.m4v",
+    id_left: "square",
+    id_right: "triangle"
+};
+
+// randomly select square left vs right
+const rand_square_selection = jsPsych.randomization.sampleWithoutReplacement([square_left, square_right], 1)[0];
+
+// print the selection for easier testing!
+console.log('left: ', rand_square_selection.id_left);
+
+// use your variable wherever things are randomly selected
+const warmup_trial_intro = {
+    type: jsPsychVideoHotspots,
+    stimulus: rand_square_selection.stimulus,
+    hotspots: [{
+            id: rand_square_selection.id_left,
+            x: 100,
+            y: 150,
+            width: 325,
+            height: 325,
+        },
+        {
+            id: rand_square_selection.id_right,
+            x: 600,
+            y: 150,
+            width: 325,
+            height: 325,
+        }
+    ],
+    hotspot_highlight_css: 'background-color: rgba(255, 0, 0, 0.3); border: 2px solid red;'
+};
+
+//https://github.com/efosterhanson/CHS_stims/blob/main/ProjectSprouts/warm_up_1_redo.mp4
+
+const square_left_redo = {
+    // stimulus: "https://raw.githubusercontent.com/efosterhanson/CHS_stims/main/ProjectSprouts/warm_up_1_redo.m4v",
+    stimulus: "https://raw.githubusercontent.com/efosterhanson/CHS_stims/main/ProjectSprouts/Adobe%20Express%20-%20ProjectSprouts_warm_up_1_redo.mp4",
+    
+    id_left: "triangle",
+    id_right: "square"
+};
+const square_right_redo = {
+    stimulus: "https://raw.githubusercontent.com/efosterhanson/CHS_stims/main/Adobe%20Express%20-%20ProjectSprouts_warm_up_2_redo.mp4",
+    id_left: "square",
+    id_right: "triangle"
+};
+
+const rand_square_redo_selection = jsPsych.randomization.sampleWithoutReplacement([square_left_redo, square_right_redo], 1)[0];
+
+
+const warmup_trial_intro_redo = {
+    type: jsPsychVideoHotspots,
+    stimulus: rand_square_redo_selection.stimulus,
+    hotspots: [{
+            id: rand_square_redo_selection.id_left,
+            x: 100,
+            y: 150,
+            width: 325,
+            height: 325,
+        },
+        {
+            id: rand_square_redo_selection.id_right,
+            x: 600,
+            y: 150,
+            width: 325,
+            height: 325,
+        }
+    ],
+    hotspot_highlight_css: 'background-color: rgba(255, 0, 0, 0.3); border: 2px solid red;'
+};
+
+const warmup_trial_intro_conditional = {
+    timeline: [warmup_trial_intro_redo],
+    conditional_function: function() {
+        const last_response = jsPsych.data.get().last(1).values()[0].response;
+
+        if (last_response === "triangle") {
+            return false; // correct, skip repeat
+        } else {
+            return true; // wrong, play it one more time
+        }
+    }
+};
+
+timeline.push(warmup_trial_intro);
+timeline.push(warmup_trial_intro_conditional);
+
 const howOld = {
   type: jsPsychHtmlButtonResponse,
   stimulus: `
